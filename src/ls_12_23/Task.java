2f374ca5f0eab0491df3a66ecf6c895865538ad9
Andrey Valevskiy <valevskii.a@gmail.com>
2023-12-05 21:10:04 +0200

    Stored procedures, functions, alters, dates

---
+++ src/ls_12_23/Task.java
@@ -0,0 +1,21 @@
+package ls_12_23;
+
+public class Task {
+
+    public static void action() {
+        //20 rows of code
+    }
+
+
+    public static void main(String[] args) {
+        action();
+    }
+}
+
+class Task1 {
+
+
+    public static void main(String[] args) {
+        //20 rows of code
+    }
+}
